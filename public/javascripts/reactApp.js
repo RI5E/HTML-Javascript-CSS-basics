2f25c59e5dda763a8370113cffed9efc6c15cde6
chzou <chzou2009@gmail.com>
2016-01-23 18:52:15 -0500

    Fixed button with React

--- public/javascripts/reactApp.js
+++ public/javascripts/reactApp.js
@@ -1,32 +1,54 @@
 var Calendar = React.createClass({
+  handleChange: function(event){
+      var date = new Date(event.target.value);
+      var date_formatted = (date.getMonth()+1) + '/' + (date.getDate()+1) + '/' + date.getFullYear();
+      this.props.setChosenDate(date_formatted);
+  },
   render: function(){
     return (
-      <input className="calendar" type="date" id="twitter_date" min="2015-01-01" max="2017-01-01" required="required" />
+      <input className="calendar" type="date" id="twitter_date" min="2015-01-01" max="2017-01-01" required="required" onChange={this.handleChange}/>
     );
   }
 });
 
 var SubmitButton = React.createClass({
+  handleClick: function(event){
+      this.props.setDisplayDate();
+  },
   render: function(){
     return(
-      <div>
-        <button id="submit_button" onclick="dateSearch()">Submit</button>
-      </div>
+      // <div>
+        <button id="submit_button" onClick={this.handleClick}>Submit</button>
+      // </div>
     );
   }
 });
 
 var Query = React.createClass({
+  getInitialState: function(){
+      return {chosenDate: 'ERROR:no_date_chosen', displayDate: ''};
+  },
   render: function() {
+    var me = this;
+    var setChosenDate = function(date) {  // updates whenever calendar value is changed
+        me.setState({chosenDate: date });
+    };
+    var setDisplayDate = function() {
+        me.setState({displayDate: 'Searching for most popular hashtags on ' + me.state.chosenDate + '...'});
+    };
     return(
       <div>
         <h1 className="query" id="query">What day do you want to look at?</h1>
-        <Calendar />
-        <SubmitButton />
+        <Calendar setChosenDate={setChosenDate}/>
+        <SubmitButton setDisplayDate={setDisplayDate}/>
+        <p>
+            {this.state.displayDate}
+        </p>
       </div>
     );
   }
 });
+
 ReactDOM.render(
   <Query />,
   document.getElementById('content')
--- public/javascripts/submitButton.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#submit_button').on('click', function(){
-        var d = new Date($('#twitter_date').val());
-        var chosen_date = (d.getMonth()+1) + "/" + (d.getDate()+1) + "/" + d.getFullYear();
-        document.getElementById("searching_msg").innerText = "Searching for most popular hashtags on " + chosen_date + "...";
-        document.getElementById("top_hashtags").innerText = "Top Hashtags:";
-    });
-});
